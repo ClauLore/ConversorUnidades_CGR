cca1c67316d762723e1bf708245284d7e4eb94b9
Claudia <claudialo.garciar@gmail.com>
2025-04-13 16:52:09 -0500

    Agregar archivos de proyecto.

---
+++ ConversorUnidades_CGR/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConversorUnidades_CGR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
